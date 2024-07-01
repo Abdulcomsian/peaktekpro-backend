afdcdcd571001743ec2b7090b23956ed2f302d76
m-sheraz <muhammad.sheraz@accrualhub.com>
2024-07-01 05:16:34 -0700

    company & roles

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,47 +5,67 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\UserRole;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:8',
-        ]);
+        DB::beginTransaction();
+        try {
+            $this->validate($request, [
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|string|min:8',
+            ]);
 
-        // Create a new user
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+            // Create a new user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
 
-        // Generate a token for the user
-        $token = $user->createToken('auth_token')->plainTextToken;
+            //Assign Role
+            $user_role = new UserRole;
+            $user_role->user_id = $user->id;
+            $user_role->company_id = 1;
+            $user_role->role_id = 1;
+            $user_role->save();
 
-        return response()->json(['user' => $user, 'token' => $token], 201);
+            // Generate a token for the user
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            DB::commit();
+            return response()->json(['user' => $user, 'token' => $token], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     public function login(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required|string',
-        ]);
+        try {
+            $this->validate($request, [
+                'email' => 'required|email',
+                'password' => 'required|string',
+            ]);
 
-        if (Auth::attempt($request->only('email', 'password'))) {
-            $user = Auth::user();
-            $token = $user->createToken('auth_token')->plainTextToken;
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
+                $token = $user->createToken('auth_token')->plainTextToken;
 
-            return response()->json(['user' => $user, 'token' => $token], 200);
+                return response()->json(['user' => $user, 'token' => $token], 200);
+            }
+
+            // Authentication failed
+            return response()->json(['message' => 'Invalid Credentials'], 422);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-
-        // Authentication failed
-        return response()->json(['message' => 'Invalid Credentials'], 422);
     }
 
     public function getUser()
---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\UserRole;
+use Illuminate\Support\Facades\Hash;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $company = new Company;
+        $company->name = 'Peak Tek';
+        $company->save();
+
+        $user = new User;
+        $user->name = 'Peak Tek';
+        $user->email = 'peaktek@gmail.com';
+        $user->password = Hash::make('Abc@123!');
+        $user->save();
+
+        $user_role = new UserRole;
+        $user_role->company_id = $company->id;
+        $user_role->user_id = $user->id;
+        $user_role->role_id = 1;
+        $user_role->save();
+    }
+}
