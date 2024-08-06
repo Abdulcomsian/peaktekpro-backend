fbe71038f64af8d64aa33899f328946849f9a95b
m-sheraz <muhammad.sheraz@accrualhub.com>
2024-08-06 11:09:33 +0500

    DJUSTOR AND OVERTURN MEETING

--- database/migrations/2024_07_10_121013_create_overturn_meetings_table.php
+++ database/migrations/2024_07_10_121013_create_overturn_meetings_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('overturn_meetings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_job_id')->constrained('company_jobs')->onDelete('cascade');
-            $table->string('name');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->string('time');
-            $table->string('date');
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('time')->nullable();
+            $table->string('date')->nullable();
             $table->text('notes')->nullable();
             $table->string('status')->default('pending');
             $table->timestamps();
