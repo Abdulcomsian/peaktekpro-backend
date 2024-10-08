4ae83ea40e8e0fc2d8321f7fb56034ddf198e742
rehmat <rehmat.ullah@dbqplabs.com>
2024-10-08 17:28:17 +0500

    fixes in sign update

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64.exe'),
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
         'timeout' => false,
         'options' => [
