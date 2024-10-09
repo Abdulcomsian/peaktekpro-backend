c110690824b496353dee8017354b0ece3c5defa6
rehmat <rehmat.ullah@dbqplabs.com>
2024-10-09 12:25:48 +0500

    bug

--- database/migrations/2024_07_22_092503_create_ready_to_builds_table.php
+++ database/migrations/2024_07_22_092503_create_ready_to_builds_table.php
@@ -20,9 +20,7 @@ return new class extends Migration
             $table->string('home_owner_email')->nullable();
             $table->string('date')->nullable();
             $table->string('notes')->nullable();
-            $table->string('attachements')->nullable();
             $table->string('status')->default('false');
-            // $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
