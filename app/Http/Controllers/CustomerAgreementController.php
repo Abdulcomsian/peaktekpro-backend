742a3829c0b5e82a40012456fadb3c49ec9c67e6
rehmat <rehmat.ullah@dbqplabs.com>
2024-10-21 14:58:12 +0500

    issue in customer aggrement Apis

--- app/Http/Controllers/CustomerAgreementController.php
+++ app/Http/Controllers/CustomerAgreementController.php
@@ -292,17 +292,18 @@ class CustomerAgreementController extends Controller
             }
 
             //Check Agreement
-            $agreement = CustomerAgreement::where('company_job_id', $jobId)->first();
+            $agreement = CustomerAgreement::with('jobSummary')
+            ->where('company_job_id', $jobId)->first();
 
             ////
-            $job_summary = CompanyJobSummary::select('id','insurance','policy_number','email','insurance_representative','claim_number')
-            ->where('company_job_id', $job->id)->first();
-            if(!$job_summary) {
-                return response()->json([
-                    'status' => 200,
-                    'message' => 'Job Summary Not Yet Created',
-                ], 200);
-            }
+            // $job_summary = CompanyJobSummary::select('id','insurance','policy_number','email','insurance_representative','claim_number')
+            // ->where('company_job_id', $job->id)->first();
+            // if(!$job_summary) {
+            //     return response()->json([
+            //         'status' => 200,
+            //         'message' => 'Job Summary Not Yet Created',
+            //     ], 200);
+            // }
 
             ///
             if(!$agreement) {
@@ -319,7 +320,7 @@ class CustomerAgreementController extends Controller
                     'message' => 'Customer Agreement Not Found',
                     'data' => [
                         'agreement' => $job_info,
-                        'jobsummary' =>  $job_summary
+                        // 'jobsummary' =>  $job_summary
                     ]
                     
                 ], 200);
@@ -338,9 +339,8 @@ class CustomerAgreementController extends Controller
             return response()->json([
                 'status' => 200,
                 'message' => 'Customer Agreement Found Successfully',
-                'data' => [
-                    'agreement' => $agreement,
-                    'jobsummary' => $job_summary,
+                'data' => [ $agreement,
+                    // 'jobsummary' => $job_summary,
                 ]
                 
             ], 200);
