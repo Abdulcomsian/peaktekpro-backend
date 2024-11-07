a431db1cd98e24ef451d90f12b1dd3e8f55b41b1
rehmat <rehmat.ullah@dbqplabs.com>
2024-11-07 18:09:37 +0500

    fix

--- app/Models/CustomerAgreement.php
+++ app/Models/CustomerAgreement.php
@@ -19,7 +19,7 @@ class CustomerAgreement extends Model
     public function isComplete()
     {
         if(!is_null($this->street) && !is_null($this->city) && !is_null($this->state) && !is_null($this->zip_code)
-        && !is_null($this->insurance) && !is_null($this->claim_number) && !is_null($this->policy_number) && !is_null($this->company_signature)
+        // && !is_null($this->insurance) && !is_null($this->claim_number) && !is_null($this->policy_number) && !is_null($this->company_signature)
         && !is_null($this->company_printed_name) && !is_null($this->company_date) && !is_null($this->customer_signature) && !is_null($this->customer_printed_name)
         && !is_null($this->customer_date)) {
             return true;
