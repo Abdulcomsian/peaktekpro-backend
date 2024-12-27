7066ad56aae4b2d0f0824f9d17de8ebf815054b8
rehmat <rehmat.ullah@dbqplabs.com>
2024-12-27 11:48:58 +0500

    changes

--- resources/views/pdf/coc.blade.php
+++ resources/views/pdf/coc.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>coc pdf</title>
     <style>
       * {
         padding: 0;
@@ -264,7 +264,7 @@
           <td>
             <h2 style="text-align: left">Notes</h2>
             <p style="line-height: 1.6">    
-              {{$coc->notes}}
+              {{$coc->notes ?? ''}}
               
             </p>
           </td>
