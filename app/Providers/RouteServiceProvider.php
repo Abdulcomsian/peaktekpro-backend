ebe1a6a0696ce6ed00756dc90771e2be9192d976
rehmat <rehmat.ullah@dbqplabs.com>
2025-01-01 18:53:47 +0500

    changes in provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,7 +45,15 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
+        // RateLimiter::for('api', function (Request $request) {
+        //     return Limit::none();
+        // });
         RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+    
+        // no limit throttle
+        RateLimiter::for('none', function (Request $request) {
             return Limit::none();
         });
     }
