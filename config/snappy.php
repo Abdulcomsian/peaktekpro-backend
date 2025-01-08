977a63e70bc1a5e42f7306fbcdc967f5ed6967aa
rehmat <rehmat.ullah@dbqplabs.com>
2025-01-08 15:51:33 +0500

    changes

--- config/snappy.php
+++ config/snappy.php
@@ -37,7 +37,7 @@
             'enabled' => true,
             // 'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64.exe'), //for production
             // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"', //for local
-            'binary' => '/usr/bin/wkhtmltopdf', //global path
+            'binary' => '/usr/bin/wkhtmltopdf', //global path 
 
 
 
