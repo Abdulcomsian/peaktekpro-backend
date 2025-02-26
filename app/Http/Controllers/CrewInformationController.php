d95a3a77026a9d98e2f2da38935c965250c884de
rehmat <rehmat.ullah@dbqplabs.com>
2025-02-26 17:39:36 +0500

    crew information apis

--- app/Http/Controllers/CrewInformationController.php
+++ app/Http/Controllers/CrewInformationController.php
@@ -3,8 +3,122 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CrewInformation;
+use App\Http\Resources\CrewInformationResource;
+use App\Models\CompanyJob;
+use Exception;
 
 class CrewInformationController extends Controller
 {
-    //
+    public function addCrewInformation(Request $request,$jobId)
+    {
+        // dd($jobId);
+        try{
+            $request->validate([
+                'build_date'=> 'nullable|string',
+                'status' => 'nullable|string',
+                'crew_name' => 'nullable|string',
+                // 'data' => 'nullable|string'
+            ]);
+
+            $companyjob = CompanyJob::find($jobId);
+            if(!$companyjob){
+                return response()->json([
+                    'status'=> true,
+                    'message' => 'company job not found'
+                ]);
+            }
+    
+            $crewInformation = CrewInformation::updateOrCreate([
+                'company_job_id' => $jobId,
+            ],
+            [
+                'company_job_id' => $jobId,
+                'build_date' => $request->build_date,
+                'status' => $request->status,
+                'crew_name' => $request->crew_name,
+                // 'data' => $request->data,
+            ]);
+    
+            return new CrewInformationResource($crewInformation);
+
+        }catch(Exception $e){
+            return response()->json([
+                'status'=> false,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+    }
+
+    public function getCrewInformation($jobId)
+    {
+        try{
+            
+            $companyjob = CompanyJob::find($jobId);
+            if(!$companyjob){
+                return response()->json([
+                    'status'=> true,
+                    'message' => 'company job not found'
+                ]);
+            }
+
+            $crewInformation = CrewInformation::where('company_job_id', $jobId)->first();
+            if($crewInformation){
+                return response()->json([
+                    'status'=> true,
+                    'message' => 'data fetched successfully',
+                    'data' => $crewInformation
+                ]);
+            }
+
+            return response()->json([
+                'status'=> true,
+                'message' => 'crew information not found',
+            ]);
+
+        }catch(\Exception $e){
+            return response()->json([
+                'status'=> false,
+                'message' => $e->getMessage()
+            ]);
+        }
+       
+
+    }
+
+    public function deleteCrewInformation($jobId)
+    {
+        try{
+            
+            $companyjob = CompanyJob::find($jobId);
+            if(!$companyjob){
+                return response()->json([
+                    'status'=> false,
+                    'message' => 'company job not found'
+                ]);
+            }
+            $crewInformation = CrewInformation::where('company_job_id',$jobId)->first();
+            if(!$crewInformation){
+                return response()->json([
+                    'status'=> true,
+                    'message' => 'crew information not found',
+                ]);
+            }
+
+            $crewInformation->delete();
+            return response()->json([
+                'status'=> true,
+                'message' => 'crew information deleted successfully',
+            ]);
+
+        }catch(\Exception $e){
+            return response()->json([
+                'status'=> false,
+                'message' => $e->getMessage()
+            ]);
+        }
+       
+
+    }
 }
--- database/migrations/2025_02_26_080435_create_crew_information_table.php
+++ database/migrations/2025_02_26_080435_create_crew_information_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('crew_information', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_job_id')->constrained('company_jobs');
             $table->string('build_date')->nullable();
             $table->string('data')->nullable();
+            $table->string('status')->nullable();
+            $table->string('crew_name')->nullable();
             $table->timestamps();
         });
     }
