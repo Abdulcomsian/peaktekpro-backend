6f6d184a342751ddc3d5fd2cde83b7bf4cf8af71
rehmat <rehmat.ullah@dbqplabs.com>
2025-05-08 20:24:32 +0500

    notes fix

--- app/Http/Controllers/RecentNoteController.php
+++ app/Http/Controllers/RecentNoteController.php
@@ -10,6 +10,54 @@ class RecentNoteController extends Controller
 {
     public function getAllRecentNotes($jobId)
     {
+        try {
+            $customer = CompanyJobSummary::select('customer_name')->where('company_job_id', $jobId)->first();
+
+            $latestNotes = 
+                DB::table('adjustor_meetings')->select('notes', 'created_at', 'company_job_id')
+                ->unionAll(
+                    DB::table('insurance_under_reviews')->select('notes', 'created_at', 'company_job_id')
+                )
+                ->unionAll(
+                    DB::table('ready_to_builds')->select('notes', 'created_at', 'company_job_id')
+                )
+                ->unionAll(
+                    DB::table('final_payment_dues')->select('notes', 'created_at', 'company_job_id')
+                )
+                ->unionAll(
+                    DB::table('claim_details')->select('notes', 'created_at', 'company_job_id')
+                )
+                ->unionAll(
+                    DB::table('won_closeds')->select('notes', 'created_at', 'company_job_id')
+                )
+                ->where('company_job_id', $jobId)
+                ->orderBy('created_at', 'desc')
+                ->limit(10)
+                ->get();
+
+            // Add customer_name to each note
+            $latestNotesWithCustomer = $latestNotes->map(function ($note) use ($customer) {
+                $note->customer_name = $customer->customer_name;
+                return $note;
+            });
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Recent Notes Fetched Successfully',
+                'data' => $latestNotesWithCustomer,
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error occurred while fetching notes',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function getAllRecentNotes1($jobId)
+    {
         try{
             $customer_name = CompanyJobSummary::select('customer_name')->where('company_job_id',$jobId)->first();
             // dd($customer_name);
