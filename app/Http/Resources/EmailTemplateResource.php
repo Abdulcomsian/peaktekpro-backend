5cae557cf2d6355a8e75874f85ea014183995ff4
rehmat <rehmat.ullah@dbqplabs.com>
2025-05-28 20:57:19 +0500

    supplier title missing fixes

---
+++ app/Http/Resources/EmailTemplateResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmailTemplateResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'company_id' => $this->company_id,
+             'title'=> $this->title,
+            'content' =>$this->content,
+            'supplier_id' =>$this->supplier_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+
+        ];
+    }
+}
